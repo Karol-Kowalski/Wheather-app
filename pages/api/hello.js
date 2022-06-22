9983c2855e1049d31b51ab6618e0b3d5e128cb58
Virion <kajko195@tlen.pl>
2022-06-22 22:24:11 +0200

    Further starting configuration

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,30 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+const AirPollution = {
+  "coord":[
+    50,
+    50
+  ],
+  "list":[
+    {
+      "dt":1605182400,
+      "main":{
+        "aqi":1
+      },
+      "components":{
+        "co":201.94053649902344,
+        "no":0.01877197064459324,
+        "no2":0.7711350917816162,
+        "o3":68.66455078125,
+        "so2":0.6407499313354492,
+        "pm2_5":0.5,
+        "pm10":0.540438711643219,
+        "nh3":0.12369127571582794
+      }
+    }
+  ]
+}
+
 export default function handler(req, res) {
   res.status(200).json({ name: 'John Doe' })
 }
