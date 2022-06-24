7be5ebcb0da78d76fbb6a5020c7b769e20099d2b
Virion <kajko195@tlen.pl>
2022-06-24 10:52:00 +0200

    get user current position from geolocation

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,14 @@
+import { useEffect, useState } from "react"
+
 export default function Page() {
+  const [coords, setCoords] = useState()
+  
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      console.log(position)
+    })
+  },[])
+
   return (
     <div>
       Hello
