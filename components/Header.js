6b9bf8aa008f28695b914a757e6eb6794778dd7d
Virion <kajko195@tlen.pl>
2022-07-08 22:22:01 +0200

    basic content of Header component

--- components/Header.js
+++ components/Header.js
@@ -3,6 +3,9 @@ import Nav from "./Nav";
 export default function Header() {
   return (
     <>
+      <button>Your location</button>
+      <input placeholder="Search loaction..."></input>
+      <Nav />
     </>
   )
 }
