1d23e296f5f93e5abcb18c21e04da71337fb5133
Virion <kajko195@tlen.pl>
2022-07-08 15:34:17 +0200

    query external API with coords and response

--- pages/api/apiAirPollution.js
+++ pages/api/apiAirPollution.js
@@ -25,7 +25,15 @@ const airPollution = {
   ]
 }
 
-export default function apiAirPollution(req, res) {
-  const data = airPollution.list[0].components
-  res.status(200).json({ ...data })
+const apiKey = process.env.PRIVATE_OPEN_WHEATHER_KEY;
+
+export default async function apiAirPollution(req, res) {
+  const { lat, lon } = req.query;
+
+  const url = `http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${apiKey}`
+  
+  const apiResponse = await fetch(url);
+  const airPollution = await apiResponse.json();
+  const data = await airPollution.list[0].components;
+  res.status(200).json({ ...data });
 }
