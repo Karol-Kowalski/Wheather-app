04a9a62d8a952fc26d33d4627bd3b8c8d3bae784
Virion <kajko195@tlen.pl>
2022-07-08 22:22:33 +0200

    basic content of Nav component

--- components/Nav.js
+++ components/Nav.js
@@ -1,7 +1,12 @@
+import Link from "next/link"
+
 export default function Nav() {
 
   return (
     <>
+      <Link href="/Wheathercast">Wheahercast</Link>
+      <Link href="/AirQuality">Air Quality</Link>
+      <Link href="/Contact">Contact</Link>
     </>
   )
 }
