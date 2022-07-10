494998b30cbab80e90127675768d6638cc852d3b
Virion <kajko195@tlen.pl>
2022-07-10 22:04:24 +0200

    create global styles

--- components/Page.js
+++ components/Page.js
@@ -1,12 +1,36 @@
-import AirQuality from "./AirQuality";
+import styled, { createGlobalStyle } from "styled-components";
 import Header from "./Header";
-import Weather from "./Weather"
 
+const GlobalStyles = createGlobalStyle`
+  html{
+    font-size: 15px;
+    --white: #dfdfdf;
+  }
+
+  body{
+    margin: 0;
+    background: #0d202b;
+    font-size: 20px;
+    color: var(--white);
+  }
+  a{
+   text-decoration: none;
+   color: var(--white);
+  }
+  button{
+    border: none;
+  }
+`;
+
+const InnerStyles = styled.div``;
 export default function Page({ children }) {
   return (
     <div>
+      <GlobalStyles />
       <Header />
-      {children}
+      <InnerStyles>
+        {children}
+      </InnerStyles>
     </div>
   )
 }
